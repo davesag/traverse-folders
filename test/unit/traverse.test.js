1509ccb8c0dcc83b9bcebbcc4c0ffa9526675279
Dave Sag <davesag@gmail.com>
2019-04-14 14:22:11 +0530

    n/a improved tests a bit

--- test/unit/traverse.test.js
+++ test/unit/traverse.test.js
@@ -3,20 +3,28 @@ const { expect } = require('chai')
 const traverse = require('../../src')
 
 describe('src/utils/traverse', () => {
-  const files = []
-  const processor = file => {
-    files.push(file)
-  }
-  const expected = [
-    'test/unit/fixtures/api/outer.js',
-    'test/unit/fixtures/api/test/inner.js'
-  ]
+  context('given a simple processor', () => {
+    const files = []
+    const processor = file => {
+      files.push(file)
+    }
+    const expected = [
+      'test/unit/fixtures/api/outer.js',
+      'test/unit/fixtures/api/test/inner.js'
+    ]
 
-  before(() => {
-    traverse('test/unit/fixtures/api', processor)
+    before(() => {
+      traverse('test/unit/fixtures/api', processor)
+    })
+
+    it('returned the expected files', () => {
+      expect(files).to.deep.equal(expected)
+    })
   })
 
-  it('returned the expected files', () => {
-    expect(files).to.deep.equal(expected)
+  context('given no processor', () => {
+    it('runs but does nothing', () => {
+      expect(traverse('test/unit/fixtures/api')).to.be.undefined
+    })
   })
 })
