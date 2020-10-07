ba6f40da272c3d075d2fea113eea39f08e9d8a14
Dave Sag <davesag@gmail.com>
2020-10-08 09:43:37 +1100

    n/a updated stryker and other dependencies

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,18 +1,13 @@
-module.exports = function (config) {
-  config.set({
-    maxConcurrentTestRunners: 2, // workaround for https://github.com/stryker-mutator/stryker/issues/1525
-    mutate: ['src/**/*.js'],
-    mutator: 'javascript',
-    packageManager: 'npm',
-    reporters: ['clear-text'],
-    testRunner: 'mocha',
-    mochaOptions: {
-      spec: ['./test/unit/**/*.test.js'],
-      require: ['./test/unitTestHelper.js']
-    },
-    transpilers: [],
-    testFramework: 'mocha',
-    coverageAnalysis: 'perTest',
-    thresholds: { high: 80, low: 70, break: null }
-  })
+module.exports = {
+  // concurrency: 2,
+  mutate: ['src/**/*.js'],
+  packageManager: 'npm',
+  reporters: ['clear-text'],
+  testRunner: 'mocha',
+  mochaOptions: {
+    spec: ['./test/unit/**/*.test.js'],
+    require: ['./test/unitTestHelper.js']
+  },
+  coverageAnalysis: 'perTest',
+  thresholds: { high: 80, low: 70, break: null }
 }
